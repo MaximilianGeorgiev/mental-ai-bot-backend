9e2d1bb31b244da99d3a01f75b687d1c45287748
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2023-11-26 13:28:32 +0200

    feat: Add admin auth strategy logic

--- src/modules/auth/admin.strategy.ts
+++ src/modules/auth/admin.strategy.ts
@@ -1,27 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Strategy } from "passport-local";
 import { PassportStrategy } from "@nestjs/passport";
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { User } from "src/types/interfaces/entities/users";
+import { UsersService } from "../users/users.service";
+import { UserRoles } from "src/types/enums/user-properties.enum";
 
 // This strategy is always chained and rejects access to admin routes
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+export class AdminStrategy extends PassportStrategy(Strategy) {
+  constructor(private userService: UsersService) {
     super();
   }
 
   async validate(
     username: string,
-    password: string,
+    _password: string,
   ): Promise<Partial<User> | null> {
-    const user = await this.authService.validateUser(username, password);
+    const userFound = await this.userService.find("username", username, false);
 
-    if (!user) {
+    if (userFound) {
+      const { role } = userFound as unknown as User;
+
+      if (role.toString() !== UserRoles.ADMIN) {
+        throw new UnauthorizedException();
+      }
+    } else {
       throw new UnauthorizedException();
     }
+
     // This return value is trivial. Passport uses it internally and attaches "user" property to the Request() decorator
     // in the controller.
-    return user;
+    const { password, ...result } = userFound as unknown as User;
+    return result;
   }
 }
